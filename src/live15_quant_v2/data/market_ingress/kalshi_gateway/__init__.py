7ba7605b5828576ee2e598fe19d849614b7b3cae
QHongboQ <161150452+QHongboQ@users.noreply.github.com>
2026-09-04 15:27:48 -0700

    Export concrete LIVE15 market scope

--- src/live15_quant_v2/data/market_ingress/kalshi_gateway/__init__.py
+++ src/live15_quant_v2/data/market_ingress/kalshi_gateway/__init__.py
@@ -1,4 +1,5 @@
 """Public V2 Kalshi Gateway and Market Identity interface."""
+
 from live15_quant_v2.data.market_ingress.kalshi_gateway.gateway import KalshiGateway
 from live15_quant_v2.data.market_ingress.kalshi_gateway.identity.models import (
     MarketScopeBinding,
@@ -12,5 +13,17 @@ from live15_quant_v2.data.market_ingress.kalshi_gateway.identity.resolver import
     MarketIdentityResolution,
     MarketIdentityResolver,
 )
+from live15_quant_v2.data.market_ingress.kalshi_gateway.identity.scope import (
+    Live15MarketScopeConfig,
+)
 
-__all__=["KalshiGateway","MarketIdentityResolution","MarketIdentityResolver","MarketScopeBinding","MarketScopePort","MarketWindow","VerifiedMarketIdentity"]
+__all__ = [
+    "KalshiGateway",
+    "Live15MarketScopeConfig",
+    "MarketIdentityResolution",
+    "MarketIdentityResolver",
+    "MarketScopeBinding",
+    "MarketScopePort",
+    "MarketWindow",
+    "VerifiedMarketIdentity",
+]
